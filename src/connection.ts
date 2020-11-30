b570dc0989757437745176fc03c767f2becdcc08
paulbtw <p-p-v-2010@hotmail.de>
2020-11-30 17:43:42 +0100

    Initial commit
    New initial commit

--- src/connection.ts
+++ src/connection.ts
@@ -2,23 +2,23 @@ import { createConnection, getConnection } from "typeorm";
 import config from "./config/ormconfig";
 
 const connection = {
-  async create() {
-    return await createConnection(config);
-  },
+	async create() {
+		return await createConnection(config);
+	},
 
-  async close() {
-    return await getConnection().close();
-  },
+	async close() {
+		return await getConnection().close();
+	},
 
-  async clear() {
-    const connection = getConnection();
-    const entities = connection.entityMetadatas;
+	async clear() {
+		const connection = getConnection();
+		const entities = connection.entityMetadatas;
 
-    entities.forEach(async (entity) => {
-      const repository = connection.getRepository(entity.name);
-      await repository.query(`DELETE FROM ${entity.tableName}`);
-    });
-  },
+		entities.forEach(async (entity) => {
+			const repository = connection.getRepository(entity.name);
+			await repository.query(`DELETE FROM ${entity.tableName}`);
+		});
+	},
 };
 
 export default connection;
